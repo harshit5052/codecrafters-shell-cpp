88fb99c8ce2cc291beb7084b8af0451838a94e02
Harshit Singh <harshit.singh@juspay.in>
2026-06-18 15:42:52 +0530

    implementing finding executable file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+namespace fs = std::filesystem;
+
+#ifdef _WIN32
+    const char PATH_DELIMITER = ';';
+#else
+    const char PATH_DELIMITER = ':';
+#endif
+
+// Helper function to check if a file has executable permissions
+bool isExecutable(const fs::path& filePath) {
+    try {
+        auto perms = fs::status(filePath).permissions();
+        
+        // Check if owner, group, or others have execute bit enabled
+        return (perms & (fs::perms::owner_exec | 
+                         fs::perms::group_exec | 
+                         fs::perms::others_exec)) != fs::perms::none;
+    } catch (const fs::filesystem_error&) {
+        return false; // Fail gracefully if permissions can't be read
+    }
+}
 
 int main() {
   for(;;){
@@ -14,7 +35,46 @@ int main() {
     if(cmnd=="echo" || cmnd=="type" || cmnd=="exit"){
         cout<<cmnd<<" is a shell builtin"<<endl;
     }else{
-        cout<<cmnd<<": not found"<<endl;
+        const char* pathEnv = std::getenv("PATH");
+        std::string pathStr(pathEnv);
+        std::stringstream ss(pathStr);
+        std::string singlePath;
+        bool executablefileavailable=false;
+        string path;
+        while (std::getline(ss, singlePath, PATH_DELIMITER)) {
+            if (singlePath.empty()) continue;
+
+            fs::path dirPath(singlePath);
+
+            // 3. Skip if the path is invalid or is not a directory
+            if (!fs::exists(dirPath) || !fs::is_directory(dirPath)) {
+                continue; 
+            }
+          
+            // 4. Iterate over files within the target directory
+            try {
+                for (const auto& entry : fs::directory_iterator(dirPath)) {
+                  if(entry.path().filename().string()==cmnd){
+                    if (fs::is_regular_file(entry.status())) {
+                        bool executable = isExecutable(entry.path());
+
+                        if(executable){
+                           executablefileavailable=true;
+                           path=entry.path();
+                        }
+                    }
+                    break;
+                  }
+                }
+            } catch (const fs::filesystem_error& e) {
+                std::cerr << "  Error reading directory: " << e.what() << "\n";
+            }
+        }
+        if(executablefileavailable){
+          cout<<cmnd<<" is "<<path<<endl;
+        }else{
+          cout<<cmnd<<": not found"<<endl;
+        }
     }
   }else{
     cout<<s<<": command not found"<<endl;
