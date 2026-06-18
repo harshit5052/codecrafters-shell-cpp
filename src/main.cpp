f366e84ccf5480e10148b95546330b98dc2f7029
Harshit Singh <harshit.singh@juspay.in>
2026-06-18 12:16:07 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::cout << "$ ";
+  cout << "$ ";
 }
