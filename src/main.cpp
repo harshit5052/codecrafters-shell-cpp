2d6023f6438c4882ed3f4b7a19f8ef7b3958df03
Harshit Singh <harshit.singh@juspay.in>
2026-06-18 14:14:30 +0530

    compilation fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
   string s;
   getline(cin, s);
   if(s=="exit")break;
-  if(s.lenth()>=5 && s.substr(0,5)=="echo "){
+  if(s.length()>=5 && s.substr(0,5)=="echo "){
     cout<<s.substr(5)<<endl;
   }else{
     cout<<s<<": command not found"<<endl;
